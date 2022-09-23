09f2cc6e9ae88b9530380235888c0bcca73f36c4
Rose Pineau <rosepineau@protonmail.com>
2022-09-23 16:37:03 -0400

    Change kermit alias.

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import { Librarian } from "./classes";
 // Globals
 const contentDiv = document.querySelector(".content");
 
-console.log("kermit is the mfn man");
+console.log("kermit is the mfn man!");
 
 // Create default project on load:
 const defaultProject = new Project("default", 1);
